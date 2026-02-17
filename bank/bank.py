e1bb1c0906d0b4083fe6894a972bf29ea41448f6
devbysamcloudy <snganga685@gmail.com>
2026-02-17 11:30:14 +0300

    update

--- bank/bank.py
+++ bank/bank.py
@@ -1,16 +1,28 @@
 #Handles all the required transactions and opening of accounts
 import json
+import os
+import sys
+
 def create_account(id_number, name):
-    account_no = "1111"
     account = {
-        "account_no": account_no,
+        "account_no": id_number,
         "id_number":id_number,
         "name":name,
         "Transaction_history": [],
         "balance": 0
     }
 
-    filename = f"{account_no}.json"
+    filename = f"accounts/{id_number}.json"
     with open (filename, "w") as file:
         json.dump(account,file)
     print(f"Account created successfully")
+
+def get_account_by_id(id_number):
+    filename = f"accounts/{id_number}.json"
+    if not os.path.exists(filename):
+        return None
+
+    with open (filename, "r") as file:
+        account = json.load(file)
+        print(account)
+    return account        
